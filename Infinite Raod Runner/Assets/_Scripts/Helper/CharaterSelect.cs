106fe2f0be36dbed7c16ac0d656d19ce566c634b
Sunil pargi <sunilpargi121@gmail.com>
2020-10-29 11:51:36 +0530

    added Gameplay UI

---
+++ Infinite Raod Runner/Assets/_Scripts/Helper/CharaterSelect.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharaterSelect : MonoBehaviour
+{
+    public GameObject[] available_Heroes;
+
+    private int currentIndex;
+
+    public Text selectedText;
+    public GameObject starIcon;
+    public Image selectBtn_Image;
+    public Sprite button_Green, button_Blue;
+
+    private bool[] heroes;
+
+    public Text starScoreText;
+	void Start()
+	{
+		InitializeCharacters();
+	}
+
+	void InitializeCharacters()
+	{
+
+	//	currentIndex = GameManager.instance.selected_Index;
+
+		for (int i = 0; i < available_Heroes.Length; i++)
+		{
+			available_Heroes[i].SetActive(false);
+		}
+		available_Heroes[currentIndex].SetActive(true);
+
+	//	heroes = GameManager.instance.heroes;
+	}
+
+	public void NextHero()
+	{
+		available_Heroes[currentIndex].SetActive(false);
+
+		if (currentIndex + 1 == available_Heroes.Length)
+		{
+			currentIndex = 0;
+
+		}
+		else
+		{
+			currentIndex++;
+		}
+
+		available_Heroes[currentIndex].SetActive(true);
+
+	//	CheckIfCharacterIsUnlocked();
+
+	}
+
+	public void PreviousHero()
+	{
+		available_Heroes[currentIndex].SetActive(false);
+
+		if (currentIndex - 1 == -1)
+		{
+			currentIndex = available_Heroes.Length - 1;
+
+		}
+		else
+		{
+			currentIndex--;
+		}
+
+		available_Heroes[currentIndex].SetActive(true);
+
+		//CheckIfCharacterIsUnlocked();
+
+	}
+}
---
+++ Infinite Raod Runner/Assets/_Scripts/Helper/MainmenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainmenuController : MonoBehaviour
+{
+    public GameObject hero_Menu;
+    public Text starScoreText;
+
+    public Image music_Img;
+    public Sprite music_Off, music_On;
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("Gameplay");
+	}
+
+	public void HeroMenu()
+	{
+		hero_Menu.SetActive(true);
+		// starScoreText.text = "" + GameManager.instance.starScore;
+	}
+
+	public void HomeButton()
+	{
+		hero_Menu.SetActive(false);
+	}
+}
